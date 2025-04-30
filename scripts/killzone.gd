cf8ed4a000a2b5ce63d96b4a2afef1eacfcdf81a
Dekoder-py <kyleyesmariam@icloud.com>
2025-05-01 08:53:19 +1200

    files

---
+++ scripts/killzone.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You died.")
+	Engine.time_scale = 0.3
+	audio_stream_player.play()
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
